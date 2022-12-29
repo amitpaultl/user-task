7ad0f7c4c3c055360974a49b5c0d2c009f001b10
amitpaultl <amitpaultl@gmail.com>
2022-12-29 19:28:06 +0600

    comment add

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const dbConnent = async () => {
 dbConnent();
 
 const task = client.db('userTask').collection('task');
+const comment = client.db('userTask').collection('comment');
 
 //  add task
 app.post("/userTask", async (req, res) => {
@@ -43,6 +44,49 @@ app.post("/userTask", async (req, res) => {
         res.send({
           success: false,
           error: error.message,
+        });
+    }
+})
+
+//  add comment
+app.post("/comment", async (req, res) => {
+    try {
+        const result = await comment.insertOne(req.body);
+        if (result.insertedId) {
+            res.send({
+              success: true,
+              message: `Successfully created the ${req.body.title}`,
+            });
+        }else {
+            res.send({
+              success: false,
+              error: "Couldn't create the product",
+            });
+        }
+    } catch (error) {
+        res.send({
+          success: false,
+          error: error.message,
+        });
+    }
+})
+
+//  get comment
+app.get("/comment", async(req,res)=>{
+    try {
+        const query = {};
+        const cursor = comment.find(query)
+        const products = await cursor.toArray();
+        res.send({
+            success: true,
+            data: products,
+            message: "Successfully got the data",
+        });
+        
+    } catch (error) {
+        res.send({
+            success: false,
+            error: error.message,
         });
     }
 })
@@ -113,6 +157,33 @@ app.put(`/updateTask/:id`,async (req,res)=>{
     }
 })
 
+// // update data
+// app.put(`/updateTaskOld/:id`,async (req,res)=>{
+//     try {
+//         const id = req.params.id;
+//         const status = req.body;
+//         // const update : {$set : status};
+//         const filter = {_id : ObjectId(id)}
+//         const updateId = {
+//             $set: {
+//                 publish: true
+//             }
+//         }
+//         const result = await task.insertOne(status);
+
+//         res.send({
+//             success : true,
+//             data: result
+//         })
+        
+//     } catch (error) {
+//         res.send({
+//             success: false,
+//             error: error.message
+
+//         })
+//     }
+// })
 
 // update data
 app.put(`/updateTaskNot/:id`,async (req,res)=>{
@@ -167,6 +238,8 @@ app.delete('/deleteTask/:id', async(req,res)=>{
     }
 
 })
+
+// 
 
 app.get('/', (req, res) => {
     res.send('user task  server running')
